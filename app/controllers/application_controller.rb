34ca0f70ecfa1c4c4e6378f2204f307b30137b6d
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-04-02 13:21:33 +0900

    ユーザー編集時にfirst_name、last_nameをDBに登録。 Deviseコントローラを継承しオーバーライド

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     edit_user_registration_path
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
 end
