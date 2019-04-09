562e3886b83d4caabadd9f06be93ea0315240d71
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-04-09 14:25:40 +0900

    Tweetモデルfetchメソッドを200件取得に変更

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,4 @@
 class Tweet < ApplicationRecord
-  require 'twitter'
   has_many :tweet_images ,dependent: :destroy
   belongs_to :user
 
@@ -14,40 +13,23 @@ class Tweet < ApplicationRecord
     #一旦全削除
     user.tweets.destroy_all
 
+    tweets = client.favorites(count: 200,tweet_mode: "extended")
 
-    def collect_with_max_id(collection=[], max_id=nil, &block)
-      response = yield(max_id)
-      collection += response
-      response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
-    end
-
-    # いいね全件取得
-    def client.get_all_favorites(user)
-      collect_with_max_id do |max_id|
-        options = {count: 200,tweet_mode:"extended"}
-        options[:max_id] = max_id unless max_id.nil?
-        favorites(user, options)
-      end
-    end
-
-
-    favorites = client.get_all_favorites(user.twitter_id)
-
-    favorites.each do |favorite|
+    tweets.each do |tweet|
       user.tweets.create(
-                             post_id: favorite.id,
-                             post_created_at: favorite.created_at,
-                             text: favorite.attrs[:full_text],
-                             favorite_count: favorite.favorite_count,
-                             retweet_count: favorite.retweet_count,
-                             postuser_id: favorite.user.id,
-                             postuser_name: favorite.user.name,
-                             postuser_screen_name: favorite.user.screen_name,
-                             profile_description: favorite.user.description
+                             post_id: tweet.id,
+                             post_created_at: tweet.created_at,
+                             text: tweet.attrs[:full_text],
+                             favorite_count: tweet.favorite_count,
+                             retweet_count: tweet.retweet_count,
+                             postuser_id: tweet.user.id,
+                             postuser_name: tweet.user.name,
+                             postuser_screen_name: tweet.user.screen_name,
+                             profile_description: tweet.user.description
       )
       #ツイートに画像があれば保存
-      if favorite.media?
-        favorite.media.each do |media|
+      if tweet.media?
+        tweet.media.each do |media|
           user.tweets.last.tweet_images.create(
                                            tweetimage_url: media.media_url_https
           )
