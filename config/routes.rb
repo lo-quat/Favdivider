2400c2a2725a354b027cc658e396487b0a7c4c18
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-04-13 14:35:17 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
   resources :users,only: [:edit,:update]
+  resources :tweets,only: [:index]
   get 'users/tweet_fetch', to: 'users#tweet_fetch'
-  get 'tweets/index', to: 'tweets#index'
   devise_for :users
   get '/auth/:provider/callback', to: 'users#callback'
 end
