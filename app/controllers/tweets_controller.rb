7d29b966bf443ac0fa1244bc94ad0e99f407ac39
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-05-02 16:05:31 +0900

    dotenv追加。ツイートとカテゴリの関連付け

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,27 +1,38 @@
 class TweetsController < ApplicationController
   def index
     if params[:search].present?
-      @tweets = Tweet.search(current_user.id, {tweet_text: params[:tweet_text],like_num: params[:like_num]})
+      @tweets = Tweet.search(current_user.id, {tweet_text: params[:tweet_text], like_num: params[:like_num]})
     elsif params[:sort]
       @tweets = current_user.tweets.order("favorite_count DESC")
     else
       @tweets = current_user.tweets
     end
-  end
 
-  def update
-    relationship = Relationship.new(category_id: params[:relationship][:category],
-                                    tweet_id: params[:relationship][:tweet_id])
-    relationship.save
-    redirect_to edit_tweet_url(params[:relationship][:tweet_id])
+    @postuser_name_all = Tweet.postuser_name_all(current_user)
   end
 
   def edit
     @tweet = current_user.tweets.find(params[:id])
+    @relationship = @tweet.relationships.new
+    #@tweet.relationships.new
+  end
+
+  def update
+    relationship = Relationship.new(tweet_params)
+
+    if relationship.save
+      redirect_to edit_tweet_url(params[:relationship][:tweet_id])
+    end
   end
 
   def clip
     Tweet.find_by(id: params[:id]).cliped!
     redirect_to(tweets_path)
+  end
+
+  private
+
+  def tweet_params
+    params.require(:relationship).permit(:category_id,:tweet_id)
   end
 end
