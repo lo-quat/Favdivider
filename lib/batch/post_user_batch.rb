7f7746b64ec208b083fbd22508d902d997a33657
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-08-25 12:03:53 +0900

    API制限を乗り越えた

--- lib/batch/post_user_batch.rb
+++ lib/batch/post_user_batch.rb
@@ -6,18 +6,18 @@ class PostUserBatch
       config.access_token = User.first.access_token
       config.access_token_secret = User.first.access_token_secret
     end
-    ids = User.first.post_users.pluck(:uid)
-    ids.map!(&:to_i)
-
-    #ユーザーがもっている投稿ユーザーの最新情報を取得
-    post_users = client.users(ids[0..99])
-    #DBの情報を更新する
-    post_users.each do |post_user|
-      _post_user = PostUser.find_by(uid: post_user.id)
-      _post_user.name = post_user.name
-      _post_user.profile_description = post_user.description
-      _post_user.profile_image = post_user.profile_image_uri_https
-      _post_user.save!
+    api_limit  = 100
+    all_ids = User.first.post_users.pluck(:uid)
+    all_ids.map!(&:to_i)
+    all_ids.each_slice(api_limit) do |ids|
+      post_users = client.users(ids)
+      post_users.each do |post_user|
+        _post_user = PostUser.find_by(uid: post_user.id)
+        _post_user.name = post_user.name
+        _post_user.profile_description = post_user.description
+        _post_user.profile_image = post_user.profile_image_uri_https
+        _post_user.save!
+      end
     end
   end
 end
