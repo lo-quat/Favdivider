5da10c90bca0edb0203c0942360fd3d8a387c4d9
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-08-25 23:23:29 +0900

    バッチ処理完成

---
+++ lib/sns/tw.rb
@@ -0,0 +1,32 @@
+module Sns::Tw
+
+  API_LIMIT = 100
+
+  def self.batch
+
+    users = User.all
+
+    users.each do |user|
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV['APP_ID']
+        config.consumer_secret = ENV['APP_SECRET']
+        config.access_token = user.access_token
+        config.access_token_secret = user.access_token_secret
+      end
+
+      all_ids = User.first.post_users.pluck(:uid)
+      all_ids.map!(&:to_i)
+      all_ids.each_slice(API_LIMIT) do |ids|
+        post_users = client.users(ids)
+        post_users.each do |post_user|
+          _post_user = PostUser.find_by(uid: post_user.id)
+          _post_user.name = post_user.name
+          _post_user.profile_description = post_user.description
+          _post_user.profile_image = post_user.profile_image_uri_https
+          _post_user.save!
+        end
+      end
+    end
+    puts 'db updated'
+  end
+end
