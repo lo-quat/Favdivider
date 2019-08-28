ef9a26071e882d9e58506430c6902c3cf3d9d4c4
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-08-28 12:45:44 +0900

    PostUserの表示順を変更

--- app/controllers/post_users_controller.rb
+++ app/controllers/post_users_controller.rb
@@ -4,7 +4,10 @@ class PostUsersController < ApplicationController
   # GET /post_users
   # GET /post_users.json
   def index
-    @post_users = current_user.post_users
+    @post_users = current_user.post_users.select('post_users.*', 'count(tweets.post_user_id) AS tweets')
+                      .left_joins(:tweets)
+                      .group('tweets.post_user_id')
+                      .order('tweets desc')
   end
 
   # GET /post_users/1
@@ -17,23 +20,24 @@ class PostUsersController < ApplicationController
   def update
     respond_to do |format|
       if @post_user.update(post_user_params)
-        format.html { redirect_to @post_user, notice: 'Post user was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post_user }
+        format.html {redirect_to @post_user, notice: 'Post user was successfully updated.'}
+        format.json {render :show, status: :ok, location: @post_user}
       else
-        format.html { render :edit }
-        format.json { render json: @post_user.errors, status: :unprocessable_entity }
+        format.html {render :edit}
+        format.json {render json: @post_user.errors, status: :unprocessable_entity}
       end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post_user
-      @post_user = PostUser.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_user_params
-      params.fetch(:post_user, {})
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post_user
+    @post_user = PostUser.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_user_params
+    params.fetch(:post_user, {})
+  end
 end
