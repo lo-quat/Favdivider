8537cfe510c2fe344912c8916e44788ef81aa782
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-10-02 16:13:53 +0900

    カテゴリー作成のバグ修正
    他ユーザーとカテゴリー名が重複しているとカテゴリーが作成できないバグを修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   has_many :relationships, dependent: :destroy
   has_many :tweets, through: :relationships, dependent: :destroy
   belongs_to :user
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: { scope: :user_id }
   enum status: { unpublish: 0, publish: 1 }
 
   def toggle_status!
