bf699813a7ceb5e10fe011a9c962667e8b162057
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-10-07 02:45:06 +0900

    共有ツイートのバグ修正
    複数カテゴリーが付いている場合の重複表示バグを修正

--- app/controllers/everybodys/tweets_controller.rb
+++ app/controllers/everybodys/tweets_controller.rb
@@ -2,6 +2,6 @@ class Everybodys::TweetsController < Everybodys::Base
   def index
     # カテゴリー付きで公開ツイートのみを抽出
     # 公開/非公開設定はカテゴリーごとに設定してある
-    @tweets = Tweet.joins(:categories).where(categories: { status: :publish })
+    @tweets = Tweet.joins(:categories).where(categories: { status: :publish }).distinct
   end
 end
