82ed9e0f30d0e19493d8efefddad1e4e2ea88309
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-10-18 23:27:45 +0900

    Add validation test to tweet_spec

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -4,16 +4,57 @@ RSpec.describe Tweet, type: :model do
 
   before do
     @user = create(:user)
+    @post_user = create(:post_user)
+  end
+
+  describe "validation" do
+    it "is valid with user_id, post_user_id and status" do
+      tweet = Tweet.new(
+          user_id: 1,
+          post_user_id: 1,
+          status: 0
+      )
+      expect(tweet).to be_valid
+    end
+
+    it "is invalid without user_id" do
+      tweet = Tweet.new(
+          user_id: nil,
+          post_user_id: 1,
+          status: 0
+      )
+      tweet.valid?
+      expect(tweet.errors[:user_id]).to include("can't be blank")
+    end
+
+    it "is invalid without post_user_id" do
+      tweet = Tweet.new(
+          user_id: 1,
+          post_user_id: nil,
+          status: 0
+      )
+      tweet.valid?
+      expect(tweet.errors[:post_user_id]).to include("can't be blank")
+    end
+
+    it "is invalid without status" do
+      tweet = Tweet.new(
+          user_id: 1,
+          post_user_id: 1,
+          status: nil
+      )
+      tweet.valid?
+      expect(tweet.errors[:status]).to include("can't be blank")
+    end
   end
 
   describe "#search" do
 
     context "テキスト検索" do
       context "検索結果が存在する場合" do
-        let!(:post_user) { create(:post_user)}
-        let!(:tweet1) {create(:tweet1)}
-        let!(:tweet2) {create(:tweet2)}
-        let!(:results){Tweet.search(@user.id, {tweet_text: 'abc'})}
+        let!(:tweet1) { create(:tweet1) }
+        let!(:tweet2) { create(:tweet2) }
+        let!(:results) { Tweet.search(@user.id, {tweet_text: 'abc'}) }
         it "件数が正しいこと" do
           expect(results.count).to eq(1)
         end
@@ -24,10 +65,9 @@ RSpec.describe Tweet, type: :model do
       end
 
       context "検索結果が存在しない場合" do
-        let!(:post_user) { create(:post_user)}
-        let!(:tweet1) {create(:tweet1)}
-        let!(:tweet2) {create(:tweet2)}
-        let!(:results){Tweet.search(@user.id, {tweet_text: 'rfb'})}
+        let!(:tweet1) { create(:tweet1) }
+        let!(:tweet2) { create(:tweet2) }
+        let!(:results) { Tweet.search(@user.id, {tweet_text: 'rfb'}) }
         it "該当ツイートが存在しないこと" do
           expect(results.count).to eq(0)
         end
@@ -36,10 +76,9 @@ RSpec.describe Tweet, type: :model do
 
     context "ライク検索" do
       context "検索結果が存在する場合" do
-        let!(:post_user) { create(:post_user)}
-        let!(:tweet1) {create(:tweet1)}
-        let!(:tweet3) {create(:tweet3)}
-        let!(:results){Tweet.search(@user.id, {like_num: 10})}
+        let!(:tweet1) { create(:tweet1) }
+        let!(:tweet3) { create(:tweet3) }
+        let!(:results) { Tweet.search(@user.id, {like_num: 10}) }
         it "件数が正しいこと" do
           expect(results.count).to eq(1)
         end
@@ -49,10 +88,9 @@ RSpec.describe Tweet, type: :model do
       end
 
       context "検索結果が存在しない場合" do
-        let!(:post_user) { create(:post_user)}
-        let!(:tweet1) {create(:tweet1)}
-        let!(:tweet3) {create(:tweet3)}
-        let!(:results){Tweet.search(@user.id, {like_num: 20})}
+        let!(:tweet1) { create(:tweet1) }
+        let!(:tweet3) { create(:tweet3) }
+        let!(:results) { Tweet.search(@user.id, {like_num: 20}) }
         it "該当ツイートが存在しないこと" do
           expect(results.count).to eq(0)
         end
@@ -61,23 +99,36 @@ RSpec.describe Tweet, type: :model do
 
     context "カテゴリー検索" do
       context "検索結果が存在する場合" do
-        let!(:post_user) { create(:post_user)}
-        let!(:tweet1){create(:tweet1)}
-        let!(:category){create(:category)}
-        let!(:relationship){create(:relationship)}
-        let!(:results){Tweet.search(@user.id,{category_id: 1})}
+        let!(:tweet1) { create(:tweet1) }
+        let!(:category) { create(:category) }
+        let!(:relationship) { create(:relationship) }
+        let!(:results) { Tweet.search(@user.id, {category_id: 1}) }
         it "件数が正しいこと" do
-          expect(results.first.id).to eq(1)
+          expect(results.count).to eq(1)
+        end
+      end
+    end
+
+    context "クリップ検索" do
+      context "検索結果が存在する場合" do
+        let!(:tweet3) { create(:tweet3) }
+        let!(:category) { create(:category) }
+        let!(:relationship) { create(:relationship) }
+        let!(:results) { Tweet.search(@user.id, {clip: 1}) }
+        it "件数が正しいこと" do
+          expect(results.count).to eq(1)
+        end
+        it "内容が正しいこと" do
+          expect(results.first.status).to eq("cliped")
         end
       end
     end
 
     context "複合検索" do
       context "検索結果が存在する場合" do
-        let!(:post_user) { create(:post_user)}
-        let!(:tweet1) {create(:tweet1)}
-        let!(:tweet3) {create(:tweet3)}
-        let!(:results){Tweet.search(@user.id, {tweet_text: 'abc',like_num: 10})}
+        let!(:tweet1) { create(:tweet1) }
+        let!(:tweet3) { create(:tweet3) }
+        let!(:results) { Tweet.search(@user.id, {tweet_text: 'abc', like_num: 10}) }
         it "件数が正しいこと" do
           expect(results.count).to eq(1)
         end
@@ -87,10 +138,9 @@ RSpec.describe Tweet, type: :model do
       end
 
       context "検索結果が存在しない場合" do
-        let!(:post_user) { create(:post_user)}
-        let!(:tweet1) {create(:tweet1)}
-        let!(:tweet3) {create(:tweet3)}
-        let!(:results){Tweet.search(@user.id,{tweet_text: 'abc',like_num: 100})}
+        let!(:tweet1) { create(:tweet1) }
+        let!(:tweet3) { create(:tweet3) }
+        let!(:results) { Tweet.search(@user.id, {tweet_text: 'abc', like_num: 100}) }
         it "該当ツイートが存在しないこと" do
           expect(results.count).to eq(0)
         end
@@ -98,11 +148,10 @@ RSpec.describe Tweet, type: :model do
     end
 
     context "検索条件の指定がない場合" do
-      let!(:post_user) { create(:post_user)}
-      let!(:tweet1) {create(:tweet1)}
-      let!(:tweet2) {create(:tweet2)}
-      let!(:tweet3) {create(:tweet3)}
-      let!(:results){Tweet.search(@user.id)}
+      let!(:tweet1) { create(:tweet1) }
+      let!(:tweet2) { create(:tweet2) }
+      let!(:tweet3) { create(:tweet3) }
+      let!(:results) { Tweet.search(@user.id) }
       it "全件のツイートが取得されること" do
         expect(results.count).to eq(3)
       end
