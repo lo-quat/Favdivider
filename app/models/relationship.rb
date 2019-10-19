537544b206bcee0db8fb0dcf60dcc28af9a2523a
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-10-19 19:24:55 +0900

    Add validation to all model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,4 +2,5 @@ class Relationship < ApplicationRecord
   belongs_to :tweet
   belongs_to :category
   validates :tweet_id, uniqueness: { scope: :category_id }
+  validates :tweet_id, :category_id, presence: true
 end
