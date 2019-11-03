5d35847895387ade8f04af76dcc3cbf6629735f7
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-11-03 14:39:31 +0900

    Improve tweets index UI

--- app/controllers/everybodys/tweets_controller.rb
+++ app/controllers/everybodys/tweets_controller.rb
@@ -2,6 +2,7 @@ class Everybodys::TweetsController < Everybodys::Base
   def index
     # カテゴリー付きで公開ツイートのみを抽出
     # 公開/非公開設定はカテゴリーごとに設定してある
-    @tweets = Tweet.joins(:categories).where(categories: { status: :publish }).distinct
+    @tweets = Tweet.joins(:categories).where(categories: { status: :publish }).distinct.page(params[:page]).per(30)
+    render template: 'tweets/index'
   end
 end
