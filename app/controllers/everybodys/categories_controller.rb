efe8b55922dde7f678b4e97b0aa40a70762519ec
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-11-13 00:40:48 +0900

    Add function #65

--- app/controllers/everybodys/categories_controller.rb
+++ app/controllers/everybodys/categories_controller.rb
@@ -2,13 +2,23 @@ class Everybodys::CategoriesController < Everybodys::Base
   before_action :set_category, only: [:show]
 
   def index
-    @categories = Category.where(status: :publish)
+    @categories = Category.where(status: :publish).group(:name).having('count(*) >= 1')
   end
 
   def show
     # 任意のカテゴリーに紐づけられたツイート一覧を表示する
-    @tweets = @category.tweets.page(params[:page]).per(30)
-    render template: 'tweets/index'
+    # 別のユーザが同じ名前のカテゴリー名を作成している場合、同じ名前でツイートをまとめる
+    same_name_category = Category.where(name: @category.name, status: :publish)
+    if same_name_category.count > 1
+      tweets = same_name_category.inject do |category, n|
+        category.tweets.or(n.tweets)
+      end
+    elsif
+      tweets = same_name_category.first.tweets
+    end
+
+    @tweets = tweets.page(params[:page]).per(30)
+    render template: 'everybodys/tweets/index'
   end
 
   private
--- app/controllers/everybodys/tweets_controller.rb
+++ app/controllers/everybodys/tweets_controller.rb
@@ -3,6 +3,6 @@ class Everybodys::TweetsController < Everybodys::Base
     # カテゴリー付きで公開ツイートのみを抽出
     # 公開/非公開設定はカテゴリーごとに設定してある
     @tweets = Tweet.joins(:categories).where(categories: { status: :publish }).distinct.page(params[:page]).per(30)
-    render template: 'tweets/index'
+    render template: 'everybodys/tweets/index'
   end
 end
