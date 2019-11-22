5c0f373bcc00f171cb80043b9744bd9b41a3c8e6
山口徳馬 <tokuma.yamaguchi@gmail.com>
2019-11-23 02:59:54 +0900

    Fix route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    Tweet.fetch(current_user) if current_user.sign_in_count == 1
-    tweets_path
+    if current_user.sign_in_count == 1
+      Tweet.fetch(current_user)
+    else
+      flash.delete(:notice)
+    end
+      tweets_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resources :users, only: %i[index show]
   end
   root 'home#top'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'users/tweet_fetch', to: 'users#tweet_fetch'
   resources :users, only: %i[edit show update destroy]
   resources :tweets, only: %i[index update edit]
@@ -12,6 +13,5 @@ Rails.application.routes.draw do
   resources :post_users, only: %i[index show]
   patch 'tweets/:id/clip', to: 'tweets#toggle_status', as: 'toggle_status'
   patch 'categories/category_publish/:id', to: 'categories#category_publish', as: 'category_publish'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'tweets/hashtag', to: 'tweets#hashtag'
 end
