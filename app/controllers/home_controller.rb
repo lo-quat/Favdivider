a98ac0710c8b1c02bd2ff3590c6cc7c9c621045e
山口徳馬 <tokuma.yamaguchi@gmail.com>
2020-09-27 17:40:43 +0900

    Add before action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   layout 'home'
+  before_action :signed_in?
 
-  def top
-    if user_signed_in?
-      redirect_to tweets_url
-    end
+  def top; end
+
+  private
+
+  def signed_in?
+    redirect_to tweets_url if user_signed_in?
   end
 end
